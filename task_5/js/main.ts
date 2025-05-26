29a5b22223b1a1af6d619b656ec38abb7ba6471c
lemmiEphraim <lemmiabdissa@gmail.com>
2025-05-26 11:27:40 +0300

    Update main.ts

--- task_5/js/main.ts
+++ task_5/js/main.ts
@@ -1,1 +1,34 @@
+// Interfaces
+interface MajorCredits {
+    credits: number;
+    _majorBrand: 'major'; // Brand property
+}
 
+interface MinorCredits {
+    credits: number;
+    _minorBrand: 'minor'; // Brand property
+}
+
+// Functions
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        _majorBrand: 'major'
+    };
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+    return {
+        credits: subject1.credits + subject2.credits,
+        _minorBrand: 'minor'
+    };
+}
+
+// Example usage
+const major1: MajorCredits = { credits: 3, _majorBrand: 'major' };
+const major2: MajorCredits = { credits: 4, _majorBrand: 'major' };
+console.log(sumMajorCredits(major1, major2));
+
+const minor1: MinorCredits = { credits: 1, _minorBrand: 'minor' };
+const minor2: MinorCredits = { credits: 2, _minorBrand: 'minor' };
+console.log(sumMinorCredits(minor1, minor2));
